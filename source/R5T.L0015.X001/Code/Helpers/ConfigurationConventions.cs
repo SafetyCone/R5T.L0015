5a4a4efb7e02d3087e9a0663bc8e4957e7c327c7
David Coats <david@getsafetycone.com>
2021-11-23 12:04:29 -0800

    Change to the Microsoft.Extensions.Configuration.Json Nuget package.

---
+++ source/R5T.L0015.X001/Code/Helpers/ConfigurationConventions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class ConfigurationConventions
+    {
+        /// <summary>
+        /// Make configuration non-optional by default.
+        /// If you have spent the effort to add the configuration, the computer should at least check that the configuration exists!
+        /// Make it easy to override the default optionality at the right point.
+        /// </summary>
+        public const bool DefaultOptionality = false;
+    }
+}
